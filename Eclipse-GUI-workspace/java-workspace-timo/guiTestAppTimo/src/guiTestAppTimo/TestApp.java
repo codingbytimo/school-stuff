de355cb92ac545576d38819e9633d540093fe060
Timo <timokoch2003@gmail.com>
2022-10-04 15:15:10 +0200

    added cmponents

--- Eclipse-GUI-workspace/java-workspace-timo/guiTestAppTimo/src/guiTestAppTimo/TestApp.java
+++ Eclipse-GUI-workspace/java-workspace-timo/guiTestAppTimo/src/guiTestAppTimo/TestApp.java
@@ -7,18 +7,36 @@ import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.JTabbedPane;
 import javax.swing.JLabel;
+import javax.imageio.ImageIO;
 import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import javax.swing.JTextArea;
 import javax.swing.event.ChangeListener;
+import javax.swing.plaf.metal.MetalLookAndFeel;
+import javax.swing.plaf.metal.OceanTheme;
 import javax.swing.JCheckBox;
 import javax.swing.JColorChooser;
 import javax.swing.JRadioButton;
 import javax.swing.JList;
 import javax.swing.JSlider;
 import javax.swing.JProgressBar;
+import javax.swing.JComboBox;
+import javax.swing.JScrollBar;
+import javax.swing.JSeparator;
+import javax.swing.JPopupMenu;
+import java.awt.Component;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+
+import javax.swing.JPasswordField;
+import javax.swing.SpringLayout;
+import javax.swing.UIManager;
 
 public class TestApp implements ActionListener {
 
@@ -27,13 +45,20 @@ public class TestApp implements ActionListener {
 	private JPanel panelBlue;
 	private JPanel panelGreen;
 	private JPanel panelSpass;
-	private JPanel panelDebug;
 	private JTabbedPane tabpane;
 	private JLabel lblGutenTag;
-	private JLabel lblDebugLog;
 	private JButton btnKnopf;
 	private JLabel label;
 	private JColorChooser farben;
+	private JComboBox comboBox;
+	
+	String comboBoxListe[] = {"Baden-Württemberg", "Bayern",
+            "Berlin", "Brandenburg", "Bremen",
+            "Hamburg", "Hessen", "Mecklenburg-Vorpommern",
+            "Niedersachsen", "Nordrhein-Westfalen", "Rheinland-Pfalz",
+            "Saarland", "Sachsen", "Sachsen-Anhalt",
+            "Schleswig-Holstein", "Thüringen"};
+	private JPasswordField passwordField;
 	
 	/**
 	 * Launch the application.
@@ -63,14 +88,13 @@ public class TestApp implements ActionListener {
 	 */
 	private void initialize() {
 		frame = new JFrame();
-		frame.setBounds(300, 100, 450, 300);
+		frame.setBounds(300, 100, 650, 500);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		
         panelRot = new JPanel();
         panelBlue = new JPanel();
         panelGreen = new JPanel();
         panelSpass = new JPanel();
-        panelDebug = new JPanel();
  
         panelRot.setBackground(Color.RED);
         panelBlue.setBackground(Color.BLUE);
@@ -82,40 +106,47 @@ public class TestApp implements ActionListener {
         tabpane.addTab("Ich bin rot", panelRot);
         panelRot.setLayout(null);
         
-        JProgressBar progressBar = new JProgressBar();
-        progressBar.setBounds(10, 94, 414, 14);
-        panelRot.add(progressBar);
-        
-        JButton btnMehr = new JButton("Mehr");
-        btnMehr.setBounds(10, 119, 89, 23);
-        btnMehr.addActionListener(e -> progressBar.setValue((int) Math.random()));
-        panelRot.add(btnMehr);
-        
         tabpane.addTab("Ich bin blau", panelBlue);
         tabpane.addTab("Ich bin grün", panelGreen);
-        tabpane.addTab("Spass", panelSpass);
-        tabpane.addTab("DEBUG", panelDebug);
-        
-        lblDebugLog = new JLabel("DEBUG Log:");
-        panelDebug.add(lblDebugLog);
-        panelSpass.setLayout(null);
+        tabpane.addTab("Komponenten", panelSpass);
         
         btnKnopf = new JButton("Knopf");
         btnKnopf.setBounds(61, 36, 85, 23);
+        btnKnopf.setForeground(Color.WHITE);
+        btnKnopf.setBackground(Color.DARK_GRAY);
         btnKnopf.addActionListener(e -> System.exit(0));
+        panelSpass.setLayout(null);
+        
+        JPopupMenu popupMenu = new JPopupMenu();
+        popupMenu.setBounds(-10008, -10053, 121, 77);
+        addPopup(panelSpass, popupMenu);
+        
+        JLabel lblDasIstEin = new JLabel("Das ist ein PopUp Menu");
+        popupMenu.add(lblDasIstEin);
+        
+        JButton btnZurck = new JButton("Noch ein Knopf");
+        popupMenu.add(btnZurck);
+        
+        JLabel lblPasswort = new JLabel("Passwort:");
+        popupMenu.add(lblPasswort);
+        
+        passwordField = new JPasswordField();
+        popupMenu.add(passwordField);
         panelSpass.add(btnKnopf);
         
         label = new JLabel("");
         label.setBounds(61, 114, 0, 0);
         panelSpass.add(label);
         
-        lblGutenTag = new JLabel("Guten Tag.");
-        lblGutenTag.setBounds(61, 11, 171, 14);
+        lblGutenTag = new JLabel("Guten Tag. Rechtsklick f\u00FCr mehr Zeugs");
+        lblGutenTag.setBounds(61, 11, 288, 14);
         panelSpass.add(lblGutenTag);
         
         JTextArea txtrTextEingebenBitte = new JTextArea();
-        txtrTextEingebenBitte.setText("Text eingeben bitte.");
         txtrTextEingebenBitte.setBounds(61, 70, 171, 22);
+        txtrTextEingebenBitte.setText("Eingabe bitte.");
+        txtrTextEingebenBitte.setForeground(Color.BLACK);
+        txtrTextEingebenBitte.setBackground(Color.WHITE);
         panelSpass.add(txtrTextEingebenBitte);
         
         JCheckBox chckbxCheck = new JCheckBox("gib Check");
@@ -128,7 +159,28 @@ public class TestApp implements ActionListener {
         
         JSlider slider = new JSlider();
         slider.setBounds(61, 155, 200, 26);
+        slider.setBackground(Color.WHITE);
         panelSpass.add(slider);
+        
+        comboBox = new JComboBox(comboBoxListe);
+        comboBox.setBounds(61, 208, 171, 20);
+        comboBox.setForeground(Color.WHITE);
+        comboBox.setBackground(Color.GRAY);
+        panelSpass.add(comboBox);
+        
+        BufferedImage myPicture;
+		try {
+			myPicture = ImageIO.read(new File("C:\\Users\\T.Kochanski.AD\\Desktop\\java-workspace-timo\\guiTestAppTimo\\src\\ressources\\spriteBottle.png"));
+			JLabel picLabel = new JLabel(new ImageIcon(myPicture));
+			panelGreen.add(picLabel);
+		} catch (IOException e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+        
+        JLabel lblBundeslnderDeutschland = new JLabel("Bundesl\u00E4nder Deutschland:");
+        lblBundeslnderDeutschland.setBounds(61, 183, 171, 14);
+        panelSpass.add(lblBundeslnderDeutschland);
         frame.getContentPane().add(tabpane);
 		
 	}
@@ -136,5 +188,22 @@ public class TestApp implements ActionListener {
 	@Override
 	public void actionPerformed(ActionEvent e) {
 		
+	}
+	private static void addPopup(Component component, final JPopupMenu popup) {
+		component.addMouseListener(new MouseAdapter() {
+			public void mousePressed(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			public void mouseReleased(MouseEvent e) {
+				if (e.isPopupTrigger()) {
+					showMenu(e);
+				}
+			}
+			private void showMenu(MouseEvent e) {
+				popup.show(e.getComponent(), e.getX(), e.getY());
+			}
+		});
 	}
 }
