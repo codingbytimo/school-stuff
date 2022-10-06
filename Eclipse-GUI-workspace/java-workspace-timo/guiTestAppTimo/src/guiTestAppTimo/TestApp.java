074283b97a19f1fc3fd0ab6264b80f7605d77751
Timo <timokoch2003@gmail.com>
2022-10-06 20:18:57 +0200

    added stuff again today

--- Eclipse-GUI-workspace/java-workspace-timo/guiTestAppTimo/src/guiTestAppTimo/TestApp.java
+++ Eclipse-GUI-workspace/java-workspace-timo/guiTestAppTimo/src/guiTestAppTimo/TestApp.java
@@ -1,7 +1,8 @@
-package javaGuiTimo;
+package src.guiTestAppTimo;
 
 import java.awt.Color;
 import java.awt.EventQueue;
+import java.awt.TextField;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
@@ -25,9 +26,11 @@ import javax.swing.JSlider;
 import javax.swing.JProgressBar;
 import javax.swing.JComboBox;
 import javax.swing.JScrollBar;
+import javax.swing.JScrollPane;
 import javax.swing.JSeparator;
 import javax.swing.JPopupMenu;
 import java.awt.Component;
+import java.awt.Cursor;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.image.BufferedImage;
@@ -37,6 +40,7 @@ import java.io.IOException;
 import javax.swing.JPasswordField;
 import javax.swing.SpringLayout;
 import javax.swing.UIManager;
+import javax.swing.JEditorPane;
 
 public class TestApp implements ActionListener {
 
@@ -51,14 +55,20 @@ public class TestApp implements ActionListener {
 	private JLabel label;
 	private JColorChooser farben;
 	private JComboBox comboBox;
+	private JLabel lblCount;
+	private TextField tfCount;
+	private JButton btnCount;
+	private JEditorPane EditorPane;
+	private int count = 0;
 	
-	String comboBoxListe[] = {"Baden-W�rttemberg", "Bayern",
+	String comboBoxListe[] = {"Baden-Wuerrttemberg", "Bayern",
             "Berlin", "Brandenburg", "Bremen",
             "Hamburg", "Hessen", "Mecklenburg-Vorpommern",
             "Niedersachsen", "Nordrhein-Westfalen", "Rheinland-Pfalz",
             "Saarland", "Sachsen", "Sachsen-Anhalt",
-            "Schleswig-Holstein", "Th�ringen"};
+            "Schleswig-Holstein", "Thueringen"};
 	private JPasswordField passwordField;
+	private JSeparator separator_1;
 	
 	/**
 	 * Launch the application.
@@ -107,14 +117,15 @@ public class TestApp implements ActionListener {
         panelRot.setLayout(null);
         
         tabpane.addTab("Ich bin blau", panelBlue);
-        tabpane.addTab("Ich bin gr�n", panelGreen);
-        tabpane.addTab("Komponenten", panelSpass);
+        tabpane.addTab("Ich bin gruen", panelGreen);
+        tabpane.addTab("einfache Komponenten", panelSpass);
         
         btnKnopf = new JButton("Knopf");
         btnKnopf.setBounds(61, 26, 85, 23);
         btnKnopf.setForeground(Color.WHITE);
         btnKnopf.setBackground(Color.DARK_GRAY);
-        btnKnopf.addActionListener(e -> System.exit(0));
+        Cursor cursor = new Cursor(Cursor.HAND_CURSOR);
+        btnKnopf.addActionListener(e -> panelSpass.setCursor(cursor));
         panelSpass.setLayout(null);
         
         JPopupMenu popupMenu = new JPopupMenu();
@@ -158,42 +169,99 @@ public class TestApp implements ActionListener {
         panelSpass.add(rdbtnPunktknopf);
         
         JSlider slider = new JSlider();
-        slider.setBounds(61, 164, 200, 26);
+        slider.setBounds(325, 30, 200, 26);
         slider.setBackground(Color.WHITE);
         panelSpass.add(slider);
         
         comboBox = new JComboBox(comboBoxListe);
-        comboBox.setBounds(61, 219, 171, 20);
+        comboBox.setBounds(325, 90, 171, 20);
         comboBox.setForeground(Color.WHITE);
         comboBox.setBackground(Color.GRAY);
         panelSpass.add(comboBox);
         
-        /*BufferedImage myPicture;
-		try {
-			myPicture = ImageIO.read(new File("C:\\Users\\T.Kochanski.AD\\Desktop\\java-workspace-timo\\guiTestAppTimo\\src\\ressources\\spriteBottle.png"));
-			JLabel picLabel = new JLabel(new ImageIcon(myPicture));
-			panelGreen.add(picLabel);
-		} catch (IOException e1) {
-			// TODO Auto-generated catch block
-			e1.printStackTrace();
-		}*/
-        
-        JLabel lblBundeslnderDeutschland = new JLabel("JComboBox Bundesländer:");
-        lblBundeslnderDeutschland.setBounds(61, 200, 171, 14);
+        JLabel lblBundeslnderDeutschland = new JLabel("JComboBox Bundeslaender:");
+        lblBundeslnderDeutschland.setBounds(325, 70, 171, 14);
         panelSpass.add(lblBundeslnderDeutschland);
         
         JLabel lblNewLabel = new JLabel("JTextField");
-        lblNewLabel.setBounds(59, 59, 87, 13);
+        lblNewLabel.setBounds(61, 55, 87, 13);
         panelSpass.add(lblNewLabel);
         
         JLabel lblNewLabel_1 = new JLabel("JSlider");
-        lblNewLabel_1.setBounds(61, 154, 45, 13);
+        lblNewLabel_1.setBounds(325, 11, 45, 13);
         panelSpass.add(lblNewLabel_1);
+        
+        JSeparator separator = new JSeparator();
+        separator.setForeground(new Color(160, 160, 160));
+        separator.setBackground(new Color(0, 0, 0));
+        separator.setBounds(35, 160, 550, 10);
+        panelSpass.add(separator);
         frame.getContentPane().add(tabpane);
         
+        JPanel panelComplex = new JPanel();
+        tabpane.addTab("komplexe Komponenten", null, panelComplex, null);
+        panelComplex.setLayout(null);
+        
+        lblCount = new JLabel("Zaehler");
+        lblCount.setSize(50, 20);
+        lblCount.setLocation(50, 25);
+        panelComplex.add(lblCount); 
+
+        tfCount = new TextField(count + "", 10);
+        tfCount.setSize(50, 20);
+        tfCount.setLocation(105, 25);
+        tfCount.setEditable(false);
+        panelComplex.add(tfCount);
+
+        btnCount = new JButton("Hochzaehlen");
+        btnCount.setSize(120, 20);
+        btnCount.setLocation(160, 25);
+        btnCount.setForeground(new Color(0, 0, 0));
+        panelComplex.add(btnCount); 
+        
+        separator_1 = new JSeparator();
+        separator_1.setBackground(new Color(0, 0, 0));
+        separator_1.setForeground(new Color(128, 128, 128));
+        separator_1.setBounds(40, 60, 540, 10);
+        panelComplex.add(separator_1);
+
+        BtnCountListener listener = new BtnCountListener();
+        btnCount.addActionListener(listener);
+        
+        EditorPane = new JEditorPane();
+        EditorPane.setLocation(15, 100);
+        EditorPane.setSize(600, 200);
+        EditorPane.setContentType("text/html");
+        EditorPane.setText("<html>\r\n"
+        		+ "<head>\r\n"
+        		+ "<style>\r\n"
+        		+ "body {background-color: orange;}\r\n"
+        		+ "h1   {color: blue;}\r\n"
+        		+ "p    {color: purple;}\r\n"
+        		+ "</style>"
+        		+ "</head>\r\n"
+        		+ "<body>\r\n"
+        		+ "\r\n"
+        		+ "<h1>Guten Tag!</h1>\r\n"
+        		+ "\r\n"
+        		+ "<p>Das hier ist HTML innerhalb von Java.</p>\r\n"
+        		+ "\r\n"
+        		+ "</body>\r\n"
+        		+ "</html>");
+        panelComplex.add(EditorPane);
         
 		
 	}
+	
+	// Das ist eine innere Klasse um den Zaehler nutzen zu koennen
+	   private class BtnCountListener implements ActionListener {
+	      @Override
+	      public void actionPerformed(ActionEvent evt) {
+	         ++count; // Wert vom Zaehler erhoehen
+	         // Wert vom Zaehler anzeigen
+	         tfCount.setText(count + ""); // 'int' zu 'String' konvertieren
+	      }
+	   }
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
